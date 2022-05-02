bf02adbe8e2e4d0d2ef42cb9ef50a1a51cdf1fa2
Maria Kagermanova <maria.kgm04@gmail.com>
2022-05-02 20:19:26 +0300

    remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+
+
+
+
+
+
+
+function getIndex(list, n) {
+  let head = list;
+  let index = 0;
+  let indexes = [];
+  while (head) {
+    if (head.value == n) indexes.push(index);
+    index++;
+    head = head.next;
+  }
+  return indexes;
+}
+
+function removeElement(list, position, index) {
+  if (position == 0) {
+    list = list.next;
+    return list;
+  }
+  else {
+    if (index == position) {
+      list = list.next;
+      return list;
+    }
+    else {
+      index++;
+      list.next = removeElement(list.next, position, index)
+    }
+  }
+  return list;
+}
+
+function removeKFromList(l, k) {
+  let toRemove = getIndex(l, k);
+  for (let i in toRemove) {
+    l = removeElement(l, toRemove[i], 0)
+    for (let j in toRemove) {
+      if (toRemove[j] > toRemove[i]) toRemove[j] = toRemove[j] - 1;
+      else if (toRemove[j] == toRemove[i] + 1) toRemove[j] = toRemove[j] - 1;
+    }
+  }
+  return l;
 }
 
 module.exports = {
