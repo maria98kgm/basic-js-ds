101713c2cc596a99422e01bd8e561b486236c9af
Maria Kagermanova <maria.kgm04@gmail.com>
2022-05-02 21:30:14 +0300

    queue

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,25 +23,6 @@ const { ListNode } = require('../extensions/list-node.js');
  * }
  */
 
- function convertArrayToList(arr) {
-  return arr.reverse().reduce((acc, cur) => {
-    if (acc) {
-      const node = new ListNode(cur);
-      node.next = acc;
-      return node;
-    }
-
-    return new ListNode(cur);
-  }, null);
-}
-
-
-
-
-
-
-
-
 function getIndex(list, n) {
   let head = list;
   let index = 0;
